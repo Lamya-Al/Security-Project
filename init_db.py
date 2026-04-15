712c5f1f80b1c1412191622feaa1174b5b4d2eee
Lamya <lalkathery2004@gmail.com>
2026-04-15 16:17:10 +0300

    Initial upload: App structure with SQL tables

---
+++ init_db.py
@@ -0,0 +1,33 @@
+#only need this file to create our Database (only run one time)
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+cursor = connection.cursor()
+
+#user table
+cursor.execute('''      
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            fname TEXT,
+            lname TEXT,
+            username TEXT UNIQUE NOT NULL,
+            password TEXT UNIQUE NOT NULL,
+            role TEXT NOT NULL DEFAULT 'user',
+            bio TEXT 
+        )
+    ''')
+
+#post table (maybe remove it?)
+cursor.execute('''      
+        CREATE TABLE IF NOT EXISTS post (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            content TEXT NOT NULL,
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,   
+            FOREIGN KEY (user_id) REFERENCES users (id)  
+             
+        )
+    ''')
+
+connection.commit()
+connection.close()
