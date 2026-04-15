ee2102ea0fca525971102d9ba55c3c9ba86cd9a1
Lamya <lalkathery2004@gmail.com>
2026-04-15 21:32:27 +0300

    init project

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import sqlite3
 from flask import Flask, render_template
 
 app=Flask(__name__)
@@ -10,3 +11,10 @@ def home():
 
 if __name__=="__main__":
     app.run(debug=True)
+
+
+#connect with the database:
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row 
+    return conn
