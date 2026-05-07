7ca4837f8f8edc06e92de43d1b74335aeaa1f352
Lamya <lalkathery2004@gmail.com>
2026-05-07 13:17:50 +0300

    -

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,8 @@
-from datetime import timedel
+if request.method == 'POST':
+        fname= request.form['firstname']
+        lname= request.form['lastname']
+        email= request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+        role = "user"
